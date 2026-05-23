9255d824b69f907a05484edf1b36506a39892b8a
naveenws <naveenws001@gmail.com>
2026-05-23 13:01:06 +0530

    Fixed Render deployment

--- app.py
+++ app.py
@@ -154,6 +154,7 @@ if __name__ == '__main__':
     # Initialize initial CSV file so app is populated out of the box
     ensure_initial_csv()
     # Run the server on host 127.0.0.1 port 5000 in debug mode
+
     port = int(os.environ.get("PORT", 5000))
 
-app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
